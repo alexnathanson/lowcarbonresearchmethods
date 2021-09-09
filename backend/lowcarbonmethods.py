7625dd2528b0547287a799f64496e81bef197efa
alexnathanson <alex@alexnathanson.com>
2021-09-09 19:25:14 -0400

    background as percentage

--- backend/lowcarbonmethods.py
+++ backend/lowcarbonmethods.py
@@ -94,7 +94,7 @@ for h in range(24):
 #the default module size is 50 watts. if the server has a different sized module it will be scaled appropriately
 moduleSize = 50 * float(getRequest(getServer + "/api/v1/chargecontroller.php?systemInfo=wattage-scaler"))
 
-powerPercentage = [100.0 * (p / moduleSize) for p in avgPVPower]
+powerPercentage = [str(100.0 * (p / moduleSize)) + "%" for p in avgPVPower]
 
 #add these average power stats to the dictionary
 for p in range(len(powerPercentage)):
